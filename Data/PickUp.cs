664b96626fe455fc498309567c0389b3174c98f0
wiredmartian <meggatym@yahoo.com>
2020-07-28 20:07:55 +0200

    pick up request model

---
+++ Data/PickUp.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace feeddcity.Data
+{
+    public class PickUpRequest
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string ContactName { get; set; }
+        public string ContactNumber { get; set; }
+        public string Notes { get; set; }
+        public PickUpStatus Status { get; set; }
+        public DateTime RequestedOn { get; set; }
+        public DateTime? ClosedOn { get; set; } 
+    }
+
+    public enum PickUpStatus
+    {
+        Pending,
+        Accepted,
+        Rejected,
+        Completed
+    }
+}
