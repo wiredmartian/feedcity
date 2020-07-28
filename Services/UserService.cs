329861ee5e9755c6a7ff47196353c63f86b304ee
wiredmartian <meggatym@yahoo.com>
2020-07-28 20:17:47 +0200

    create user

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Security.Cryptography;
+using Dapper;
+using feeddcity.Common;
 using feeddcity.Data;
 using feeddcity.Interfaces;
 using feeddcity.Models.User;
@@ -6,9 +10,35 @@ namespace feeddcity.Services
 {
     public class UserService: IUser
     {
+        private readonly ICommon _common;
+        public UserService(ICommon common)
+        {
+            _common = common;
+        }
         public int CreateUser(CreateUserModel user)
         {
-            throw new System.NotImplementedException();
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            var rfcBytes = new Rfc2898DeriveBytes(user.Password, salt, 10000);
+            byte[] hash = rfcBytes.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            string hashedPassword = Convert.ToBase64String(hashBytes);
+            
+            const string sql = "INSERT INTO Users (FirstName, LastName, Email, UserName, HashedPassword) VALUES (@FirstName, @LastName, @Email, @UserName, @HashedPassword);";
+            var connection = _common.GetConnection();
+            connection.Open();
+            int affectedRows = connection.Execute(sql, new
+            {
+                user.FirstName,
+                user.LastName,
+                Email = user.EmailAddress,
+                UserName = user.EmailAddress,
+                hashedPassword
+            });
+            connection.Close();
+            return affectedRows;
         }
 
         public User GetUser(string emailAddress)
