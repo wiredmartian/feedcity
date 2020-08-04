0992306b160533e2071adc7fa4c80a01243b2e95
wiredmartian <meggatym@yahoo.com>
2020-08-04 22:50:53 +0200

    validation

--- Models/PickUp/PickUpModels.cs
+++ Models/PickUp/PickUpModels.cs
@@ -5,16 +5,25 @@ namespace feeddcity.Models.PickUp
     public class PickUpRequestModel
     {
         [Required(AllowEmptyStrings = false)]
+        [MaxLength(255)]
         public string Location { get; set; }
+        
         [Required(AllowEmptyStrings = false)]
+        [MaxLength(50)]
         public string Latitude { get; set; }
+        
         [Required(AllowEmptyStrings = false)]
+        [MaxLength(50)]
         public string Longitude { get; set; }
+        
         [Required(AllowEmptyStrings = false)]
+        [MaxLength(255)]
         public string ContactName { get; set; }
+        
         [DataType(DataType.PhoneNumber)]
         [Required(AllowEmptyStrings = false)]
         public string ContactNumber { get; set; }
+        
         [DataType(DataType.MultilineText)]
         [Required(AllowEmptyStrings = false)]
         public string Notes { get; set; }
