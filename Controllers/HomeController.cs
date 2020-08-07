0d30f9db52bdbc4b997adfc90aadf8ae7f2700d6
wiredmartian <meggatym@yahoo.com>
2020-08-07 23:32:34 +0200

    home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ namespace feeddcity.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
+        [Route("home")]
         public IActionResult Index()
         {
             return Ok("Welcome");
