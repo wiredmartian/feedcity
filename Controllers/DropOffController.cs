8659e8104ed6dbc1d3fddbbf4c4a26f819592cbb
wiredmartian <meggatym@yahoo.com>
2020-08-16 22:42:49 +0200

    drop off zone controller

--- Controllers/DropOffController.cs
+++ Controllers/DropOffController.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Data.SqlClient;
+using System.Linq;
 using feeddcity.Interfaces;
+using feeddcity.Models.DropOff;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace feeddcity.Controllers
 {
     [ApiController]
+    [Authorize]
     [Route("api/[controller]")]
     public class DropOffController : Controller
     {
@@ -12,6 +18,34 @@ namespace feeddcity.Controllers
         public DropOffController(IDropOff dropOff)
         {
             _dropOff = dropOff;
+        }
+
+        public IActionResult CreateDropOffZone([FromBody] DropOffZoneModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState.Values.SelectMany(err => err.Errors[0].ErrorMessage));
+                }
+
+                var affectedRows = _dropOff.CreateDropOff(model);
+                if (affectedRows == 0)
+                {
+                    return BadRequest(new {error = "Failed to create zone"});
+                }
+
+                return Ok(new { message = "New drop off zone created!"});
+            }
+            catch (SqlException sqlException)
+            {
+                Console.WriteLine(sqlException);
+                return StatusCode(500, new {error = "Something went horribly wrong"});
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new {message = e.Message});
+            }
         }
     }
 }
