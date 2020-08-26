e7d064cc70461e0cef8469785e56bb184c1aecf4
wiredmartian <meggatym@yahoo.com>
2020-08-26 21:45:36 +0200

    imports update

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Authentication;
